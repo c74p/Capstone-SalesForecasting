cf7a86924cb198dbb1ee5f7625ba0d69ef4497fd
chachi <chachi@localhost.localdomain>
2019-01-10 16:00:09 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Sales Forecasting and Data Science project on the Rossmann (German retailer) data set',
+    author='Craig Paulette',
+    license='MIT',
+)
