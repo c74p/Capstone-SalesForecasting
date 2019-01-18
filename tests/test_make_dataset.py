36bb9e3f04efc75e8e2d84805369cce7e24b5446
chachi <chachi@localhost.localdomain>
2019-01-18 17:12:09 -0500

    Working on creating strategies for Hypothesis test on merge_csvs. Test broken where it needs to be updated.

--- tests/test_make_dataset.py
+++ tests/test_make_dataset.py
@@ -1,9 +1,16 @@
+import datetime
+from hypothesis import assume, given
+from hypothesis.extra.pandas import column, data_frames
+from hypothesis.strategies import datetimes, sampled_from
 import pandas as pd
 from src.data import make_dataset
 from unittest import TestCase, mock
-from hypothesis import given
-from hypothesis.extra.pandas import column, data_frames
-from hypothesis.strategies import sampled_from
+
+# This is the test file for the src/data/make_dataset.py file.
+# Note that it uses the object-oriented unittest style for some parts, and
+# the stripped-down function approach (after definition of appropriate
+# strategies) for property-based testing in Hypothesis. It may be a little
+# jarring, but I found the function approach easier to use in Hypothesis.
 
 
 class test_Import_Csvs(TestCase):
@@ -19,26 +26,6 @@ class test_Import_Csvs(TestCase):
         # These may be used in the import_csvs_* series of tests below
         self.fake_empty_files = []
         self.fake_empty_read = ['', '', '']
-
-        # Config dataframe strategies for hypothesis testing
-        # These may be used in the merge_csvs_* series of tests below
-        self.goog_file_vals = ["Rossmann_DE", "Rossmann_DE_BE",
-                               "Rossmann_DE_BW", "Rossmann_DE_BY",
-                               "Rossmann_DE_HE", "Rossmann_DE_HH",
-                               "Rossmann_DE_NI", "Rossmann_DE_NW",
-                               "Rossmann_DE_RP", "Rossmann_DE_SH",
-                               "Rossmann_DE_SL", "Rossmann_DE_SN",
-                               "Rossmann_DE_ST", "Rossmann_DE_TH"
-                               ]
-
-        self.google_raw = data_frames([
-            column('file', elements=sampled_from(self.goog_file_vals)),
-            column('week', dtype=pd.Timestamp),
-            column('trend', elements=sampled_from(range(0, 101)))])
-
-        # file     2072 non-null object
-        # week     2072 non-null object
-        # trend    2072 non-null int64
 
     def tearDown(self):
         pass
@@ -75,48 +62,99 @@ class test_Import_Csvs(TestCase):
                 read = make_dataset.import_csvs('bogus_dir',
                                                 ignore_files=['b.csv'])
                 assert read == {'a.csv': '', 'c.csv': ''}
-                # assert 'ignore_files' not in kwargs
+                # how do I assert 'ignore_files' not in kwargs when the
+                # function is called?
                 mock_pandas.assert_called_with('bogus_dir/c.csv')
 
-    @given(self.google_raw())
-    def test_test_of_hypothesis(self, df):
-        assert self.df['file'].dtype == object
 
-    def test_merge_csvs(self):
+# Config dataframe strategies for hypothesis testing
+# These may be used in the merge_csvs_* series of tests below
 
-        self.testorama = data_frames([
-                                      column('A', dtype=int),
-                                      column('B', dtype=float),
-                                      column('C', dtype=object)]
-                                     )
-        pass
-    # Want merge_all_csvs() to:
-    # - merge all the csvs together into one, appropriately
-    # What could go wrong?
-    # - Not all the csvs could be there
-    # - bunch of NaNs
-    # - files could incorrectly come together
 
-    def test_verify_csv_pull(self):
-        pass
-    # Want verify_csv_pull() to:
-    # - Check the csv pull and send a message to user
-    #   - Either pull was successful, or pull failed, why, and what to do next
-    # What could go wrong?
-    # - Not all the csvs could be there
+# Configuration and strategy for googletrend.csv file
+google_file_vals = ["Rossmann_DE", "Rossmann_DE_BE", "Rossmann_DE_BW",
+                    "Rossmann_DE_BY", "Rossmann_DE_HE", "Rossmann_DE_HH",
+                    "Rossmann_DE_NI", "Rossmann_DE_NW", "Rossmann_DE_RP",
+                    "Rossmann_DE_SH", "Rossmann_DE_SL", "Rossmann_DE_SN",
+                    "Rossmann_DE_ST", "Rossmann_DE_TH"]
 
-    def test_XXX_Test_Name(self):
-        pass
-        # raise NotImplementedError('Insert test code here.')
-        #  Examples:
-        # self.assertEqual(fp.readline(), 'This is a test')
-        # self.assertFalse(os.path.exists('a'))
-        # self.assertTrue(os.path.exists('a'))
-        # self.assertTrue('already a backup server' in c.stderr)
-        # self.assertIn('fun', 'disfunctional')
-        # self.assertNotIn('crazy', 'disfunctional')
-        # with self.assertRaises(Exception):
-        #   raise Exception('test')
-        #
-        # Unconditionally fail, e.g. in a try block that should raise
-        # self.fail('Exception was not raised')
+google_strat = data_frames([
+    column('file', elements=sampled_from(google_file_vals)),
+    column('week', elements=datetimes(
+        min_value=datetime.datetime(2000, 1, 1, 0, 0, 0),
+        max_value=datetime.datetime(2018, 12, 31, 11, 59, 59))),
+    column('trend', elements=sampled_from(range(0, 101)))])
+
+# Configuration for state_names.csv file
+# Since this file is crucial to structuring the merged pdf, it's hard-coded
+state_names = ["BadenWuerttemberg", "Bayern", "Berlin", "Brandenburg",
+               "Bremen", "Hamburg", "Hessen", "MecklenburgVorpommern",
+               "Niedersachsen", "NordrheinWestfalen", "RheinlandPfalz",
+               "Saarland", "Sachsen", "SachsenAnhalt", "SchleswigHolstein",
+               "Thueringen"]
+
+state_abbreviations = ["BB", "BE", "BW", "BY", "HB", "HB,NI", "HE", "HH", "MV",
+                       "NW", "RP", "SH", "SL", "SN", "ST", "TH"]
+
+state_names_df = pd.DataFrame({'StateName': state_names,
+                              'State': state_abbreviations})
+
+# Configuration and strategy for store.csv file
+google_file_vals = ["Rossmann_DE", "Rossmann_DE_BE", "Rossmann_DE_BW",
+                    "Rossmann_DE_BY", "Rossmann_DE_HE", "Rossmann_DE_HH",
+                    "Rossmann_DE_NI", "Rossmann_DE_NW", "Rossmann_DE_RP",
+                    "Rossmann_DE_SH", "Rossmann_DE_SL", "Rossmann_DE_SN",
+                    "Rossmann_DE_ST", "Rossmann_DE_TH"]
+
+# EDIT -- FIX THIS!!!
+assert False, 'Fix here!'
+stores_strat = data_frames([
+    column('Store', elements=sampled_from(range(1, 1116))),
+    column('StoreType', elements=sampled_from(['a', 'b', 'c', 'd'])),
+    column('Assortment', elements=sampled_from(['a', 'b', 'c'])),
+    column('trend', elements=sampled_from(range(0, 101)))])
+
+
+@given(google_strat)
+def test_test_of_hypothesis(df):
+    assume(len(df) > 0)
+    assert df['file'].dtype == object
+    assert df['week'].dtype == '<M8[ns]'
+    df['week'] = df['week'].dt.strftime('%Y-%m-%d')
+    assert df['week'].dtype == object
+
+
+def test_merge_csvs():
+    pass
+# Want merge_all_csvs() to:
+# - merge all the csvs together into one, appropriately
+# What could go wrong?
+# - Not all the csvs could be there
+# - bunch of NaNs
+# - files could incorrectly come together
+
+
+def test_verify_csv_pull():
+    pass
+# Want verify_csv_pull() to:
+# - Check the csv pull and send a message to user
+#   - Either pull was successful, or pull failed, why, and what to do next
+# What could go wrong?
+# - Not all the csvs could be there
+
+
+def test_XXX_Test_Name():
+    pass
+    # raise NotImplementedError('Insert test code here.')
+    #  Examples:
+    # self.assertEqual(fp.readline(), 'This is a test')
+    # self.assertFalse(os.path.exists('a'))
+    # self.assertTrue(os.path.exists('a'))
+    # self.assertTrue('already a backup server' in c.stderr)
+    # self.assertIn('fun', 'disfunctional')
+    # self.assertNotIn('crazy', 'disfunctional')
+    # with self.assertRaises(Exception):
+    #   raise Exception('test')
+    #
+    # Unconditionally fail, e.g. in a try block that should raise
+    # self.fail('Exception was not raised')
