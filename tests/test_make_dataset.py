d1fa9b1eee451572add0a813ce80c4d74af06bdc
c74p <c74@pm.me>
2019-01-22 12:06:20 -0500

    Updated tests/test_make_dataset.py, still not done

--- tests/test_make_dataset.py
+++ tests/test_make_dataset.py
@@ -1,7 +1,8 @@
 import datetime
 from hypothesis import assume, given
 from hypothesis.extra.pandas import column, data_frames
-from hypothesis.strategies import datetimes, sampled_from
+from hypothesis.strategies import datetimes, just, one_of, sampled_from
+import numpy as np
 import pandas as pd
 from src.data import make_dataset
 from unittest import TestCase, mock
@@ -100,19 +101,87 @@ state_names_df = pd.DataFrame({'StateName': state_names,
                               'State': state_abbreviations})
 
 # Configuration and strategy for store.csv file
-google_file_vals = ["Rossmann_DE", "Rossmann_DE_BE", "Rossmann_DE_BW",
-                    "Rossmann_DE_BY", "Rossmann_DE_HE", "Rossmann_DE_HH",
-                    "Rossmann_DE_NI", "Rossmann_DE_NW", "Rossmann_DE_RP",
-                    "Rossmann_DE_SH", "Rossmann_DE_SL", "Rossmann_DE_SN",
-                    "Rossmann_DE_ST", "Rossmann_DE_TH"]
-
-# EDIT -- FIX THIS!!!
-assert False, 'Fix here!'
 stores_strat = data_frames([
     column('Store', elements=sampled_from(range(1, 1116))),
     column('StoreType', elements=sampled_from(['a', 'b', 'c', 'd'])),
     column('Assortment', elements=sampled_from(['a', 'b', 'c'])),
-    column('trend', elements=sampled_from(range(0, 101)))])
+    column('CompetitionDistance',
+           elements=one_of(just(np.nan),
+                           [float(i) for i in range(0, 100000)])),
+    column('CompetitionOpenSinceMonth',
+           elements=one_of(just(np.nan),
+                           [float(i) for i in range(1, 13)])),
+    column('CompetitionOpenSinceYear',
+           elements=one_of(just(np.nan),
+                           [float(i) for i in range(1900, 2016)])),
+    column('Promo2', elements=sampled_from([0, 1])),
+    column('Promo2SinceWeek',
+           elements=one_of(just(np.nan),
+                           [float(i) for i in range(0, 51)])),
+    column('Promo2SinceYear',
+           elements=one_of(just(np.nan),
+                           [float(i) for i in range(2009, 2016)])),
+    column('PromoInterval', elements=sampled_from(['Feb,May,Aug,Nov',
+                                                   'Jan,Apr,Jul,Oct',
+                                                   'Mar,Jun,Sept,Dec',
+                                                   np.nan
+                                                   ]))
+    ])
+
+# Configuration and strategy for store_states.csv file
+store_states_strat = data_frames([
+    column('Store', elements=sampled_from(range(1, 1116))),
+    column('State', elements=sampled_from(["BE", "BW", "BY", "HB,NI", "HE",
+                                           "HH", "NW", "RP", "SH", "SN", "ST",
+                                           "TH"]))
+    ])
+
+# Configuration and strategy for train.csv file
+train_strat = data_frames([
+    column('Store', elements=sampled_from(range(1, 1116))),
+    column('DayOfWeek', elements=sampled_from(range(1, 8))),
+    column('Date', elements=one_of([d.strftime('%Y-%m-%d') for d in
+                                    range('2013-01-01', '2015-07-31')])),
+    column('Sales', elements=sampled_from(range(0, 50000))),
+    column('Customers', elements=sampled_from(range(0, 10000))),
+    column('Open', elements=one_of([0, 1])),
+    column('Promo', elements=one_of([0, 1])),
+    column('StateHoliday', elements=sampled_from('0', 'a', 'b', 'c')),
+    column('SchoolHoliday', elements=one_of([0, 1]))
+    ])
+
+# Configuration and strategy for weather.csv file
+weather_strat = data_frames([
+    column('file', elements=sampled_from(state_names)),
+    column('date', elements=range('2013-01-01 00:00:00',
+                                  '2015-09-17 00:00:00')),
+    column('Max_TemperatureC', elements=sampled_from(range(-20, 45))),
+    column('Min_TemperatureC', elements=sampled_from(range(-20, 45))),
+    column('Dew_PointC', elements=sampled_from(range(-20, 45))),
+    column('MeanDew_PointC', elements=sampled_from(range(-20, 45))),
+    column('MinDew_PointC', elements=sampled_from(range(-80, 45))),
+    column('Max_Humidity', elements=sampled_from(range(20, 100))),
+    column('Mean_Humidity', elements=sampled_from(range(20, 100))),
+    column('Min_Humidity', elements=sampled_from(range(0, 100))),
+    column('Max_Sea_Level_PressurehPa', elements=sampled_from(range(900,
+                                                                    1100))),
+    column('Mean_Sea_Level_PressurehPa', elements=sampled_from(range(900,
+                                                                     1100))),
+    column('Min_Sea_Level_PressurehPa', elements=sampled_from(range(900,
+                                                                    1100))),
+    column('Max_VisibilityKm',
+           elements=sampled_from([float(i) for i in range(0, 40)])),
+    column('Mean_VisibilityKm',
+           elements=sampled_from([float(i) for i in range(0, 40)])),
+    column('Min_VisibilitykM',
+           elements=sampled_from([float(i) for i in range(0, 40)])),
+    column('Max_Wind_SpeedKm_h', elements=sampled_from(range(0, 110))),
+    column('Mean_Wind_SpeedKm_h', elements=sampled_from(range(0, 110))),
+    column('Max_Gust_SpeedKm_h',
+           elements=sampled_from([float(i) for i in range(10, 120)]))  # ,
+    # Check Max_Gust_SpeedKm_h; add Precipitationmm, CloudCover, Events, and
+    # WindDirDegrees
+    ])
 
 
 @given(google_strat)
