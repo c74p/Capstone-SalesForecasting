6b722b7ee81843c28a3f19e08fc5d1efde65696c
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-03-04 21:45:09 +0000

    Started to add get_pred_new_model to src/models/train_model.py. Unit tests fail.

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+from fastai import callbacks
 from fastai.basic_train import Learner, load_learner
-from fastai.tabular import DatasetType, TabularList
+from fastai.metrics import exp_rmspe
+from fastai.tabular import DatasetType, FloatList, tabular_learner, TabularList
+from fastai.train import fit_one_cycle
+from functools import partial
 import math
 import numpy as np
 import pandas as pd
@@ -10,7 +15,7 @@ DATA_PATH = Path('../data/interim')
 MODELS_PATH = Path('../models/')
 
 ERR_MSG = \
-"""USAGE: \n Option 1: -test_value=<INT> where 0 <= INT <="""
+    """USAGE: \n Option 1: -test_value=<INT> where 0 <= INT <="""
 """41608\n An optional flag of '-context' will also"""
 """provide the actual value for comparison.\n Option 2: """
 """-new_value=<FILENAME> where <FILENAME> is a .csv file"""
@@ -18,12 +23,14 @@ ERR_MSG = \
 """data."""
 
 
-MAX_TEST_VALUE=41608
-MIN_TEST_VALUE=0
+MAX_TEST_VALUE = 41608
+MIN_TEST_VALUE = 0
+
 
 def rmspe(predicted: np.array, actual: np.array) -> float:
     """Root mean squared percentage error"""
     return np.sqrt((((actual - predicted)/actual)**2).sum()/len(actual))
+
 
 def get_pred_new_data_old_model(valid_df: pd.DataFrame, path: Path) -> float:
     """Get a RSMPE score for predictions from the existing best model, with
@@ -33,11 +40,65 @@ def get_pred_new_data_old_model(valid_df: pd.DataFrame, path: Path) -> float:
     Output: the root mean squared percentage error for the predicted sales.
     """
     valid_df = preprocess.preprocess(valid_df)
-    learn = load_learner(MODELS_PATH,
-                         test=TabularList.from_df(valid_df, path=MODELS_PATH))
+    learn = load_learner(path, test=TabularList.from_df(valid_df, path=path))
 
     # get log predictions and compare to actual values
     log_preds, _ = learn.get_preds(ds_type=DatasetType.Test)
     valid_preds = np.exp(np.array(log_preds.flatten()))
     valid_reals = valid_df.loc[valid_df.sales != 0, 'sales'].values
     return rmspe(valid_preds, valid_reals)
+
+def get_pred_new_model(train_df: pd.DataFrame, valid_df: pd.DataFrame,
+                       path: Path) -> float:
+    """TODO
+    """
+
+    # Put the dataframes together and process, with valid_idx just being the
+    # portion that came from valid_df
+    df = train_df.append(valid_df).copy()
+    df = preprocess.preprocess(df)
+    args = preprocess.gather_args(df)
+    valid_idx = (len(train_df), len(df))
+
+    # Create a databunch in the usual way
+    data = (TabularList.from_df(df, path=path, cat_names=args['cat_names'],
+                                cont_names=args['cont_names'],
+                                procs=args['procs'])
+            .split_by_idx(valid_idx)
+            .label_from_df(cols=args['dep_var'],
+                           label_cls=FloatList, log=True)
+            .databunch())
+
+    # Create a learner
+    # Let's construct the learner from scratch here, in case we want to change
+    # the architecture later (we can and should - this is very basic atm)
+    learn = tabular_learner(data, layers=[100, 100], ps=[0.001, 0.01],
+                            emb_drop=0.01, metrics=exp_rmspe, y_range=None,
+                            callback_fns=[partial(callbacks.tracker.
+                                                  TrackerCallback,
+                                                  monitor='exp_rmspe'),
+                                          partial(callbacks.tracker.
+                                                  EarlyStoppingCallback,
+                                                  mode='min',
+                                                  monitor='exp_rmspe',
+                                                  min_delta=0.01, patience=1),
+                                          partial(callbacks.tracker.
+                                                  SaveModelCallback,
+                                                  monitor='exp_rmspe',
+                                                  mode='min',
+                                                  every='improvement',
+                                                  name=datetime.now().
+                                                  strftime("%Y-%m-%d-%X"))])
+
+    # Since repeated model runs showed us that 1e-3 was a good maximum learning
+    # rate for this model and since we're doing a no-human-intervention run,
+    # we'll use 1e-3 for this model. While this model is in place, we can run
+    # some offline tests as needed to see whether the maximum learning rate
+    # should be changed, but in most cases the 1e-3 is probably good, even if
+    # the model changes (again, we can test offline and update if needed).
+
+    # Also, since we have the early-stopping callback with the save-model
+    # callback set to 'every=improvement', we'll run 10 cycles even though we
+    # probably won't need nearly that many
+    print(learn.fit_one_cycle)
+    learn.fit_one_cycle(cyc_len=10, max_lr=1e-3)
--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -1,6 +1,3 @@
-#from fastai.tabular import *
-from hypothesis import given, example
-from hypothesis.strategies import text
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -8,19 +5,19 @@ import pytest
 from unittest import TestCase, mock
 from unittest.mock import patch
 
-import sys, os # NOQA
+import sys, os  # NOQA
 THIS_PATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, THIS_PATH + '/../')
 from src.models import preprocess, train_model # NOQA
 
 # This is the test file for the src/models/train_model.py file.
 
-#pd.set_option('mode.chained_assignment', 'raise')  # Chained assmt = Exception
+# pd.set_option('mode.chained_assignment', 'raise')  # Chained assmt=Exception
 
 PROJ_ROOT = Path('..')
 
 
-class Test_train_model(TestCase):
+class TestTrainModel(TestCase):
     """Test the steps needed to train the model, compare against the current
     best model, and update the 'current best' if appropriate.
     """
@@ -51,43 +48,43 @@ class Test_train_model(TestCase):
                                   'customers', 'open', 'promo',
                                   'state_holiday', 'school_holiday',
                                   'trend', 'week_start'],
-                         data=[[1, 'HE', '2015-06-20', 17, 14, 11, 9, 7, 5, 88, 64,
-                                37, 1021, 1020, 1018, 31.0, 11.0, 10.0, 21, 13,
-                                40.0, 0.0, 6.0, 'Rain', 290, 'c', 'a', 1270.0, 9.0,
-                                2008.0, 0, 23.595446584938703,
+                         data=[[1, 'HE', '2015-06-20', 17, 14, 11, 9, 7, 5, 88,
+                                64, 37, 1021, 1020, 1018, 31.0, 11.0, 10.0, 21,
+                                13, 40.0, 0.0, 6.0, 'Rain', 290, 'c', 'a',
+                                1270.0, 9.0, 2008.0, 0, 23.595446584938703,
                                 2011.7635726795095, 'None', 5, 4097.0, 494.0,
                                 1.0, 0.0, 0, 1, 85, '2015-06-14'],
-                               [1, 'HE', '2015-06-20', 17, 14, 11, 9, 7, 5, 88, 64,
-                                37, 1021, 1020, 1018, 31.0, 11.0, 10.0, 21, 13,
-                                40.0, 0.0, 6.0, 'Rain', 290, 'c', 'b', 1270.0, 9.0,
-                                2008.0, 0, 23.595446584938703,
+                               [1, 'HE', '2015-06-20', 17, 14, 11, 9, 7, 5, 88,
+                                64, 37, 1021, 1020, 1018, 31.0, 11.0, 10.0, 21,
+                                13, 40.0, 0.0, 6.0, 'Rain', 290, 'c', 'b',
+                                1270.0, 9.0, 2008.0, 0, 23.595446584938703,
                                 2011.7635726795095, 'None', 5, 4097.0, 494.0,
                                 1.0, 0.0, 0, 1, 85, '2015-06-14'],
-                               [1, 'HE', '2015-06-20', 17, 14, 11, 9, 7, 5, 88, 64,
-                                37, 1021, 1020, 1018, 31.0, 11.0, 10.0, 21, 13,
-                                40.0, 0.0, 6.0, 'Rain', 290, 'c', 'c', 1270.0, 9.0,
-                                2008.0, 0, 23.595446584938703,
+                               [1, 'HE', '2015-06-20', 17, 14, 11, 9, 7, 5, 88,
+                                64, 37, 1021, 1020, 1018, 31.0, 11.0, 10.0, 21,
+                                13, 40.0, 0.0, 6.0, 'Rain', 290, 'c', 'c',
+                                1270.0, 9.0, 2008.0, 0, 23.595446584938703,
                                 2011.7635726795095, 'None', 5, 4097.0, 494.0,
                                 1.0, 0.0, 0, 1, 85, '2015-06-14']])
 
         # Error message for an incorrect call to predict()
-        self.ERR_MSG = \
-        """USAGE: \n update-model=[<train>, <valid>] where <train> and <valid>
-        are path names to train and validation sets."""
+        self.err_msg = \
+            """USAGE: \n update-model=[<train>, <valid>] where <train> and
+            <valid> are path names to train and validation sets."""
 
         # Data path for the test data
         # I recognize these file names and PATH_NAMES are kind of contradictory
-        self.TRAIN_DATA_PATH = Path('../data/interim/train_valid_data.csv')
-        self.VALID_DATA_PATH = Path('../data/interim/test_data.csv')
-        self.valid_df = pd.read_csv(self.VALID_DATA_PATH, low_memory=False)
+        self.train_data_path = Path('../data/interim/train_valid_data.csv')
+        self.valid_data_path = Path('../data/interim/test_data.csv')
+        self.valid_df = pd.read_csv(self.valid_data_path, low_memory=False)
 
         # Path for the models
-        self.MODEL_PATH = Path('../models')
+        self.model_path = Path('../models')
 
     def tearDown(self):
         pass
 
-    def test_rmspe(self):
+    def test_rmspe(self):  # NOQA
         """Here are some hard-coded values, rather than generated, for speed"""
         assert train_model.rmspe(np.array([0]), np.array([1])) == 1
         assert train_model.rmspe(np.array([0.25]), np.array([1])) == 0.75
@@ -96,15 +93,14 @@ class Test_train_model(TestCase):
         assert train_model.rmspe(np.array([1]), np.array([0])) == np.inf
 
     def test_get_pred_new_data_old_model(self):
-        """Dumb reference test checking versuse the actual. This will need to
+        """Dumb reference test checking versus the actual. This will need to
         be updated later when the model gets updated, or it will likely fail.
         """
         # First test the actual results (will have to throw this out or update
         # later when the test gets updated)
         res = train_model.get_pred_new_data_old_model(self.valid_df,
-                                                      self.MODEL_PATH)
+                                                      self.model_path)
         assert abs(res - 0.048635791657389196) < 0.001
-
 
     @patch('src.models.preprocess.preprocess')
     @patch('src.models.train_model.load_learner')
@@ -115,15 +111,14 @@ class Test_train_model(TestCase):
         model gauged by the new data vs actuals.
         """
         with self.assertRaises(ValueError):
-        # It raises because we don't pass enough info to 'learn' to call
-        # .get_preds() - that's why this test is split into two parts
-            res = train_model.get_pred_new_data_old_model(self.df,
-                                                          self.MODEL_PATH)
+            # It raises because we don't pass enough info to 'learn' to call
+            # .get_preds() - that's why this test is split into two parts
+            train_model.get_pred_new_data_old_model(self.df, self.model_path)
         assert mock_preprocess.called
         assert mock_load_learner.called
 
     @patch('src.models.train_model.Learner.get_preds',
-           return_value=(np.array([1,1,1]),1))
+           return_value=(np.array([1, 1, 1]), 1))
     @patch('src.models.train_model.rmspe', return_value=1)
     def test_get_pred_new_data_old_model_calls_pt2(self, mock_rmspe,
                                                    mock_get_preds):
@@ -131,15 +126,57 @@ class Test_train_model(TestCase):
         The old model should be pulled in, and the accuracy of the old
         model gauged by the new data vs actuals.
         """
-        res = train_model.get_pred_new_data_old_model(self.df,
-                                                      self.MODEL_PATH)
+        train_model.get_pred_new_data_old_model(self.df, self.model_path)
         assert mock_get_preds.called
         assert mock_rmspe.called
 
-#def test_import_csvs_pulls_no_csvs_from_empty_directory(self):
-        #"""Nothing should be returned from an empty directory"""
-        #with mock.patch('os.listdir', return_value=self.fake_empty_files):
-            #with mock.patch('pandas.read_csv',
-                            #side_effect=self.fake_empty_read):
-                #read = make_dataset.import_csvs('bogus_dir')
-                #assert read == {}
+    @patch('src.models.preprocess.preprocess')
+    @patch('src.models.preprocess.gather_args')
+    @patch('src.models.train_model.TabularList')
+    @patch('src.models.train_model.tabular_learner')
+    #@patch('src.models.train_model.fit_one_cycle')
+    def test_get_pred_new_model_calls_pt1(self, # mock_fit_one_cycle,
+                                          mock_tabular_learner,
+                                          mock_tabular_list,
+                                          mock_gather_args, mock_preprocess):
+        """Calling this in parts to avoid having to make a complicated mock.
+        The data should be processed, the model run, and the new accuracy
+        calculated.
+        """
+        df1 = self.df.copy()
+        #train_model.get_pred_new_model(train_df=self.df[:2],
+        #                               valid_df=self.df[2:],
+        train_model.get_pred_new_model(train_df=df1[:2],
+                                       valid_df=df1[2:],
+                                       path=self.model_path)
+        assert mock_preprocess.called
+        assert mock_gather_args.called
+        assert mock_tabular_list.from_df.called
+        assert mock_tabular_learner.fit_one_cycle.called
+        # TODO edit these if needed and lines 15-16 rows above
+        assert mock_fit_one_cycle.called
+        # assert False
+
+    # @patch('src.models.train_model.fit_one_cycle')
+    # def test_get_pred_new_model_calls_pt2(self, mock_fit_one_cycle):
+    # def test_get_pred_new_model_calls_pt2(self):
+        """Calling this in parts to avoid having to make a complicated mock.
+        The data should be processed, the model run, and the new accuracy
+        calculated.
+        """
+        # df2 = self.df.copy()
+        #train_model.get_pred_new_model(train_df=self.df[:2],
+        #                               valid_df=self.df[2:],
+        # train_model.get_pred_new_model(train_df=df2[:2],
+        #                                valid_df=df2[2:],
+        #                                path=self.model_path)
+        # assert mock_fit_one_cycle.called
+        # assert False
+
+# def test_import_csvs_pulls_no_csvs_from_empty_directory(self):
+# """Nothing should be returned from an empty directory"""
+# with mock.patch('os.listdir', return_value=self.fake_empty_files):
+# with mock.patch('pandas.read_csv',
+# side_effect=self.fake_empty_read):
+# read = make_dataset.import_csvs('bogus_dir')
+# assert read == {}
